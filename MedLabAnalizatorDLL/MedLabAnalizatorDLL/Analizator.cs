39c7a453959c90771d20bc5d44b2f226c9f165ee
Licht <waky18050@gmail.com>
2022-03-26 08:26:44 +0300

    Добавлена библиотека

---
+++ MedLabAnalizatorDLL/MedLabAnalizatorDLL/Analizator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedLabAnalizatorDLL
+{
+    public class Analizator
+    {
+        static public string ResultAnalizator(string idService, string analizator)
+        {
+            DBConnection.Connect();
+
+            DBConnection.mycommand.CommandText = @"SELECT analizator
+            FROM services
+            WHERE IdService = '" + idService + "'";
+            object result = DBConnection.mycommand.ExecuteScalar();
+
+            if (result.ToString() != analizator)
+            {
+                return "Не верно выбран анализатор";
+            }
+
+            Random rnd = new Random();
+
+            int res = rnd.Next(0, 100);
+
+            if (res > -1 && res < 21)
+            {
+                return "Сбой исследования";
+            }
+
+            if (res > 20 && res < 41)
+            {
+                return "Плохое качество биоматериала";
+            }
+
+            return res.ToString();
+        }
+    }
+}
---
+++ MedLabAnalizatorDLL/MedLabAnalizatorDLL/DBConnection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Net;
+using System.Windows;
+
+namespace MedLabAnalizatorDLL
+{
+    public class DBConnection
+    {
+        static string connectionstring = "Database = medlab; Datasource = localhost; userid = root; password = qwerty; charset = utf8";
+
+        static public MySqlConnection myconnection;
+
+        static public MySqlCommand mycommand;
+
+        static public MySqlDataAdapter msDataAdapter;
+
+        static public bool Connect()
+        {
+            try
+            {
+                myconnection = new MySqlConnection(connectionstring);
+                myconnection.Open();
+
+                mycommand = new MySqlCommand();
+                mycommand.Connection = myconnection;
+                msDataAdapter = new MySqlDataAdapter(mycommand);
+                return true;
+            }
+            catch
+            {
+                //MessageBox.Show("Ошибка при подключении к БД", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+        }
+
+        static public void Disconnect()
+        {
+            myconnection.Close();
+        }
+    }
+}
